557467b853e5b9f865a800bc8139c1fb2987482d
luca-tomasetti <lt@bikefinder.com>
2024-03-09 10:25:46 +0100

    created script for DS

---
+++ Preprocess/create_ds.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+from PIL import Image
+
+def convert_to_jpeg(image_path):
+    img = Image.open(image_path)
+    img.mode = 'I'
+    img.point(lambda i:i*(1./256)).convert('L')
+    return img
+
+def create_folder_structure(source_dir, destination_dir):
+    for root, dirs, files in os.walk(source_dir):
+        for dirname in dirs:
+            patient_dir = os.path.join(destination_dir, dirname)
+            os.makedirs(patient_dir, exist_ok=True)
+
+        for filename in files:
+            if filename.endswith('.tiff'):
+                patient_slice_dir = os.path.join(destination_dir, os.path.basename(os.path.dirname(root)))
+                os.makedirs(patient_slice_dir, exist_ok=True)
+                patient_slice_subdir = os.path.join(patient_slice_dir, os.path.basename(root))
+                os.makedirs(patient_slice_subdir, exist_ok=True)
+                source_image_path = os.path.join(root, filename)
+                dest_image_path = os.path.join(patient_slice_subdir, filename)
+                shutil.copy(source_image_path, dest_image_path)
+
+                # Convert to JPEG
+                image = convert_to_jpeg(dest_image_path)
+                dest_jpeg_path = os.path.splitext(dest_image_path)[0] + '.jpg'
+                image.save(dest_jpeg_path)
+                os.remove(dest_image_path)
+
+
+def main():
+    source_dir = '/home/prosjekt/PerfusionCT/StrokeSUS/ORIGINAL/IMAGE_REGISTERED/'
+    destination_dir = '/home/prosjekt/PerfusionCT/StrokeSUS/GAN_project_2023/ORIGINAL_JPEG/'
+
+    create_folder_structure(source_dir, destination_dir)
+
+if __name__ == "__main__":
+    main()
